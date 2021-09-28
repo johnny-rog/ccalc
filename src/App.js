f3a98f8691b708c5944ad928b6da85fc0de490b7
johnny-rog <83927423+johnny-rog@users.noreply.github.com>
2021-09-28 19:27:29 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <html>
+        <head>
+        </head>
+        <body>
+
+        </body>
+      </html>
+
+    </div>
+  );
+}
+
+export default App;
